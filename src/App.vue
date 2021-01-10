27d1afabbd56946107366b3d271bc8cebe53c85a
Harald Moritz <harald.moritz@htl.rennweg.at>
2021-01-10 22:43:50 +0100

    Changed to table instead of card view.

--- src/App.vue
+++ src/App.vue
@@ -1,113 +1,242 @@
 <template>
   <div id="app">
     <main>
-      <b-container>
-        <b-navbar toggleable="lg" type="dark" variant="info">
-          <b-nav-form class="search-box">
-            <b-form-input
-              type="text"
-              class="search-bar"
-              placeholder="Search..."
-              v-model="query"
-            />
-          </b-nav-form>
+      <b-container fluid>
+        <!-- User Interface controls -->
+        <b-row>
+          <!--<b-col lg="6" class="my-1">
+            <b-form-group
+              label="Sort"
+              label-for="sort-by-select"
+              label-cols-sm="3"
+              label-align-sm="right"
+              label-size="sm"
+              class="mb-0"
+              v-slot="{ ariaDescribedby }"
+            >
+              <b-input-group size="sm">
+                <b-form-select
+                  id="sort-by-select"
+                  v-model="sortBy"
+                  :options="sortOptions"
+                  :aria-describedby="ariaDescribedby"
+                  class="w-75"
+                >
+                  <template #first>
+                    <option value="">-- none --</option>
+                  </template>
+                </b-form-select>
 
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                <b-form-select
+                  v-model="sortDesc"
+                  :disabled="!sortBy"
+                  :aria-describedby="ariaDescribedby"
+                  size="sm"
+                  class="w-25"
+                >
+                  <option :value="false">Asc</option>
+                  <option :value="true">Desc</option>
+                </b-form-select>
+              </b-input-group>
+            </b-form-group>
+          </b-col>-->
 
-          <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-              <b-nav-form class="sorter" id="location" @click="sort('location')">
-                <font-awesome-icon :icon="['fas', 'map-marker-alt']" />
-                <font-awesome-icon :icon="['fa', 'sort']" />
-              </b-nav-form>
-              <b-nav-form class="sorter" id="city">
-                <font-awesome-icon :icon="['fas', 'map']" />
-                <font-awesome-icon :icon="['fa', 'sort-up']" />
-              </b-nav-form>
-              <b-nav-form class="sorter" id="value">
-                <font-awesome-icon :icon="['fas', 'wind']" />
-                <font-awesome-icon :icon="['fa', 'sort']" />
-              </b-nav-form>
-              <b-nav-form class="sorter" id="coordinate">
-                <font-awesome-icon :icon="['fas', 'globe']" />
-                <font-awesome-icon :icon="['fa', 'sort']" />
-              </b-nav-form>
-              <b-nav-form class="sorter" id="lastUpdated">
-                <font-awesome-icon :icon="['fas', 'calendar-alt']" />
-                <font-awesome-icon :icon="['fa', 'sort']" />
-              </b-nav-form>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-navbar>
+          <!--<b-col lg="6" class="my-1">
+            <b-form-group
+              label="Initial sort"
+              label-for="initial-sort-select"
+              label-cols-sm="3"
+              label-align-sm="right"
+              label-size="sm"
+              class="mb-0"
+            >
+              <b-form-select
+                id="initial-sort-select"
+                v-model="sortDirection"
+                :options="['asc', 'desc', 'last']"
+                size="sm"
+              ></b-form-select>
+            </b-form-group>
+          </b-col>-->
 
-        <b-row class="measurement-wrap">
-          <b-col>
-            <b-card-group
-              class="group"
-              deck>
-              <b-card
-                class="pollution-wrap"
-                no-body
-                v-for="location in data"
-                :key="location.location"
-              >
+          <b-col lg="6" class="my-1">
+            <b-form-group
+              label="Filter"
+              label-for="filter-input"
+              label-cols-sm="3"
+              label-align-sm="right"
+              label-size="sm"
+              class="mb-0"
+            >
+              <b-input-group size="sm">
+                <b-form-input
+                  id="filter-input"
+                  v-model="filter"
+                  type="search"
+                  placeholder="Type to Search"
+                ></b-form-input>
 
-                <b-card-header class="location">
-                  <font-awesome-icon :icon="['fas', 'map-marker-alt']" />
-                  {{ location.location }}
-                </b-card-header>
-                <b-card-sub-title class="city">
-                    <font-awesome-icon :icon="['fas', 'map']" />
-                    {{ location.city }}, {{ location.country }}
-                </b-card-sub-title>
-                <b-card-body class="pollution-box" v-if="location.measurements">
-                  <font-awesome-icon :icon="['fas', 'wind']" />
-                  {{ location.measurements[0].parameter }}:
-                  {{ location.measurements[0].value }}
-                  {{ location.measurements[0].unit }}
-                </b-card-body>
-                <b-card-footer>
-                  <div class="coordinate-box" v-if="location.coordinates">
-                    <font-awesome-icon :icon="['fas', 'globe']" />
-                    {{ location.coordinates.longitude }}
-                    |
-                    {{ location.coordinates.latitude }}
-                  </div>
-                  <div class="date-box" v-if="location.measurements[0].lastUpdated">
-                    <font-awesome-icon :icon="['fas', 'calendar-alt']" />
-                    {{ dateBuilder(location.measurements[0].lastUpdated) }}
-                  </div>
-                </b-card-footer>
-              </b-card>
-            </b-card-group>
+                <b-input-group-append>
+                  <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+                </b-input-group-append>
+              </b-input-group>
+            </b-form-group>
           </b-col>
+
+          <!--<b-col lg="6" class="my-1">
+            <b-form-group
+              v-model="sortDirection"
+              label="Filter On"
+              description="Leave all unchecked to filter on all data"
+              label-cols-sm="3"
+              label-align-sm="right"
+              label-size="sm"
+              class="mb-0"
+              v-slot="{ ariaDescribedby }"
+            >
+              <b-form-checkbox-group
+                v-model="filterOn"
+                :aria-describedby="ariaDescribedby"
+                class="mt-1"
+              >
+                <b-form-checkbox value="name">Name</b-form-checkbox>
+                <b-form-checkbox value="age">Age</b-form-checkbox>
+                <b-form-checkbox value="isActive">Active</b-form-checkbox>
+              </b-form-checkbox-group>
+            </b-form-group>
+          </b-col>-->
+
+          <!--<b-col sm="5" md="6" class="my-1">
+            <b-form-group
+              label="Per page"
+              label-for="per-page-select"
+              label-cols-sm="6"
+              label-cols-md="4"
+              label-cols-lg="3"
+              label-align-sm="right"
+              label-size="sm"
+              class="mb-0"
+            >
+              <b-form-select
+                id="per-page-select"
+                v-model="perPage"
+                :options="pageOptions"
+                size="sm"
+              ></b-form-select>
+            </b-form-group>
+          </b-col>
+
+          <b-col sm="7" md="6" class="my-1">
+            <b-pagination
+              v-model="currentPage"
+              :total-rows="totalRows"
+              :per-page="perPage"
+              align="fill"
+              size="sm"
+              class="my-0"
+            ></b-pagination>
+          </b-col>-->
         </b-row>
-        <div v-infinite-scroll="reload" infinite-scroll-disabled="busy" infinite-scroll-immediate-check>
-          <!-- https://www.npmjs.com/package/vue-infinite-scroll -->
-        </div>
+
+        <!-- Main table element -->
+        <b-table
+          :items="items"
+          :fields="fields"
+          :current-page="currentPage"
+          :per-page="perPage"
+          :filter="filter"
+          :filter-included-fields="filterOn"
+          :sort-by.sync="sortBy"
+          :sort-desc.sync="sortDesc"
+          :sort-direction="sortDirection"
+          stacked="md"
+          show-empty
+          small
+          @filtered="onFiltered"
+
+          sticky-header
+          head-variant="light"
+          sort-icon-left
+          outlined
+        >
+          <!--<template #table-busy>
+            <div class="text-center text-danger my-2">
+              <b-spinner class="align-middle"></b-spinner>
+              <strong>Loading...</strong>
+            </div>
+          </template>-->
+        </b-table>
+        <div
+          v-infinite-scroll="loadMore"
+          infinite-scroll-disabled="busy"
+          infinite-scroll-distance="10"></div>
       </b-container>
     </main>
   </div>
 </template>
 
 <script>
-// import { getter } from '@/backend/axiosHelper.js'
-// import axios from 'axios'
 const OpenAQ = require('openaq')
 const client = new OpenAQ()
-// const client = new openaq
 
 export default {
   name: 'App',
   data () {
     return {
-      url_base: 'https://api.openaq.org/v1/latest/',
-      query: '',
-      data: {},
-      page: 1,
-      limit: 100,
       busy: false,
-      sorter: ['location', 'city', 'value', 'coordinates', 'lastUpdated']
+
+      items: [],
+      fields: [
+        { key: 'location', label: 'Location', sortable: true, sortDirection: 'desc' },
+        { key: 'city', label: 'City', sortable: true, class: 'text-center' },
+        { key: 'country', label: 'Country', sortable: true, class: 'text-center' },
+        {
+          key: 'measurement',
+          label: 'Measurement',
+          formatter: (value, key, item) => {
+            return this.measurementFormatter(value)
+          },
+          sortable: true,
+          sortByFormatted: true,
+          filterByFormatted: true
+        },
+        {
+          key: 'coordinates',
+          label: 'Coordinates',
+          formatter: (value, key, item) => {
+            return this.coordinatesFormatter(value)
+          },
+          sortable: true
+        },
+        {
+          key: 'lastUpdated',
+          label: 'Date',
+          formatter: (value, key, item) => {
+            return this.dateBuilder(value)
+          },
+          sortable: true,
+          sortByFormatted: false,
+          filterByFormatted: true
+        }
+      ],
+      totalRows: 1,
+      currentPage: 1,
+      perPage: 5,
+      pageOptions: [5, 10, 15, { value: 100, text: 'Show a lot' }],
+      sortBy: '',
+      sortDesc: false,
+      sortDirection: 'asc',
+      filter: null,
+      filterOn: ['city', 'country']
+    }
+  },
+  computed: {
+    sortOptions () {
+      return this.fields
+        .filter(f => f.sortable)
+        .map(f => {
+          return { text: f.label, value: f.key }
+        })
     }
   },
   watch: {
@@ -116,121 +245,47 @@ export default {
     }
   },
   mounted () {
+    this.totalRows = this.items.length
     this.reload()
   },
   methods: {
+    onFiltered (filteredItems) {
+      this.totalRows = filteredItems.length
+      this.currentPage = 1
+    },
     reload () {
-      this.busy = true
+      if (!this.busy) {
+        this.busy = true
 
-      this.data = {
-        'Loading...': {
-          location: 'Loading',
-          measurements: [{ unit: 'ug/m³', value: 1, parameter: 'co', lastUpdated: '2021-01-10T22:59:00.000Z' }],
-          city: 'St. Pölten',
-          country: 'AT'
-        }
-      }
-      /*
-      this.data = {
-        Belgium123: {
-          location: 'Belgium123',
-          measurements: [{ unit: 'pma', value: 2.0, parameter: 'CO', lastUpdated: '2021-01-09T10:27:01.501Z' }],
-          coordinates: { latitude: 3, longitude: 4 },
-          city: 'Brussel',
-          country: 'Belgium'
-        },
-        FH: {
-          location: 'FH',
-          measurements: [{ unit: 'pma', value: 2.0, parameter: 'CO', lastUpdated: '2020-11-12T10:27:01.501Z' }],
-          coordinates: { latitude: 3, longitude: 4 },
-          city: 'Vienna',
-          country: 'Austria'
-        },
-        'Oxford Campus': {
-          location: 'Oxford Campus',
-          measurements: [{ unit: 'ppma', value: 6.0, parameter: 'O2', lastUpdated: '2020-11-30T07:03:01.501Z' }],
-          coordinates: { latitude: -43, longitude: 56 },
-          city: 'Oxford',
-          country: 'GB'
-        },
-        'Oxford Hall2': {
-          location: 'Oxford Hall2',
-          measurements: [{ unit: 'ppma', value: 6.0, parameter: 'O2', lastUpdated: '2020-11-30T07:03:01.501Z' }],
-          coordinates: { latitude: -43, longitude: 56 },
-          city: 'Oxford',
-          country: 'GB'
-        },
-        'Oxford Hall3': {
-          location: 'Oxford Hall3',
-          measurements: [{ unit: 'ppma', value: 6.0, parameter: 'O2', lastUpdated: '2020-11-30T07:03:01.501Z' }],
-          coordinates: { latitude: -43, longitude: 56 },
-          city: 'Oxford',
-          country: 'GB'
-        },
-        'Oxford Hall4': {
-          location: 'Oxford Hall4',
-          measurements: [{ unit: 'ppma', value: 6.0, parameter: 'O2', lastUpdated: '2020-11-30T07:03:01.501Z' }],
-          coordinates: { latitude: -43, longitude: 56 },
-          city: 'Oxford',
-          country: 'GB'
-        },
-        'Oxford Hall5': {
-          location: 'Oxford Hall5',
-          measurements: [{ unit: 'ppma', value: 6.0, parameter: 'O2', lastUpdated: '2020-11-30T07:03:01.501Z' }],
-          coordinates: { latitude: -43, longitude: 56 },
-          city: 'Oxford',
-          country: 'GB'
-        }
-      }
-       */
-
-      // https://medium.com/dev-genius/why-is-my-fetch-request-to-oauth-server-being-blocked-by-cors-c8bdadb92b30
-      // Cross origin wird durch Browser verboten, deshalb Backend
-
-      console.log('HERE')
-      client.latest().then(data => {
-        console.log(data.results)
-        this.data = data.results
-      })
-
-      this.busy = false
-    },
-    /*
-    async get_data () {
-
-      const url = `${this.url_base}?limit=${this.limit}&page=${this.page}`
-      console.log(url)
-      const res = await axios.get(url)
-      this.data = res.data
-    },
-    /*
-    @keypress="fetchPollution"
-
-    fetchPollution (e) {
-      if (e.key === 'Enter') {
-        this.data = {}
-        fetch(`${this.url_base}?city=${this.query}`)
-          .then(res => {
-            return res.json()
-          })
-          .then(this.appendResults)
+        client.latest({
+          limit: this.perPage,
+          page: this.currentPage
+        }).then(data => {
+          console.log(data.results)
+          for (const set of data.results) {
+            let element = {}
+            element = { city: set.city, coordinates: set.coordinates, country: set.country, location: set.location }
+            element.measurement = {
+              unit: set.measurements[0].unit,
+              parameter: set.measurements[0].parameter,
+              value: set.measurements[0].value
+            }
+            element.lastUpdated = set.measurements[0].lastUpdated
+            this.items.push(element)
+          }
+          console.log(this.items)
+          for (const i of this.items) {
+            console.log(i.measurement.parameter)
+          }
+          this.busy = false
+        })
       }
     },
-    getQuery () {
-      return ''
+    loadMore () {
+      console.log('Loading additional')
+      this.currentPage += 1
+      this.reload()
     },
-    appendResults (results) {
-      console.log(results)
-      console.log(results.results)
-      if (this.data.length === undefined) {
-        this.data = results.results
-      } else {
-        for (const i in results.results) {
-          this.data.push(results.results[i])
-        }
-      }
-    },
-     */
     dateBuilder (temp) {
       const d = new Date(temp)
       const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
@@ -241,19 +296,23 @@ export default {
       const month = months[d.getMonth()]
       const year = d.getFullYear()
 
-      const hour = d.getHours()
-      const minute = d.getMinutes()
+      const hour = ('00' + d.getHours()).slice(-2)
+      const minute = ('00' + d.getMinutes()).slice(-2)
 
       return `${day}, ${date} ${month}, ${year} ${hour}:${minute}`
     },
-    sort (parameter) {
-      console.log(parameter)
-      for (const field of this.sorter) {
-        if (field !== parameter) {
-          const form = document.getElementById(field)
-          const icon = form.childNodes[1]
-          console.log(icon)
-        }
+    measurementFormatter (value) {
+      if (value !== null) {
+        return value.parameter + ': ' + value.value + value.unit
+      } else {
+        return '\xa0'
+      }
+    },
+    coordinatesFormatter (coordinates) {
+      if (coordinates !== null) {
+        return coordinates.longitude + ' | ' + coordinates.latitude
+      } else {
+        return '\xa0'
       }
     }
   }
